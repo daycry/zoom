cdb96cb9e015c371fa84b786b5fd872947da14e1
daycry <daycry9@gmail.com>
2020-04-13 15:09:59 +0200

    Update Zoom.php

--- src/Config/Zoom.php
+++ src/Config/Zoom.php
@@ -4,9 +4,9 @@ use CodeIgniter\Config\BaseConfig;
 
 class Zoom extends BaseConfig
 {
-    public $clientId = 'c5n3MF0JSiKfZxROXR3AeA';
+    public $clientId = 'xxxxxxx';
     
-    public $clientSecret = '6xoSpQntO747QISdWAaHzcspU7ozhC38';
+    public $clientSecret = 'xxxxxxxx';
 
     public $redirectUrl = 'http://zoom.local/home/zoom/';
 }
